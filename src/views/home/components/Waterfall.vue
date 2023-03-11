160351e9b9066a31ce3b05ad61c7cd9c522e5add
云冥 <1921517233@qq.com>
2023-03-11 23:04:51 +0800

    对上拉加载进行节流优化

--- src/views/home/components/Waterfall.vue
+++ src/views/home/components/Waterfall.vue
@@ -16,7 +16,6 @@
 <script lang="ts" setup>
 import { onMounted, onUnmounted, watch, ref, nextTick } from 'vue'
 import Card from './Card.vue'
-
 interface RowDataList {
   title: string
   coverImage: string
@@ -27,13 +26,11 @@ interface RowDataList {
     nickname: string
   }
 }
-
 type FirstHandleData<T, U = number> = {
   data: T
   leftOffset?: U
   rightOffset?: U
 }
-
 type SecondHandleData<T, U = number> = {
   data: T
   leftOffset?: U
@@ -42,7 +39,6 @@ type SecondHandleData<T, U = number> = {
   rightData: T
   height: U
 }
-
 type Records<T, U> = {
   data: T
   height: U
@@ -51,7 +47,6 @@ type Records<T, U> = {
   leftOffset?: U
   rightOffset?: U
 }[]
-
 let tem = [
   {
     title: '我的初秋报到妆 | 分享初秋妆容 送唇釉',
@@ -154,33 +149,25 @@ let tem = [
     }
   }
 ]
-
-
 const offsetTop = (offset: number) => {
   return offset > 0
     ? 'top: -' + offset + 'px'
     : ''
 }
-
-
 const defaultArticles = ref<RowDataList[]>([...tem])
 const io = ref<IntersectionObserver>()
 const records = ref<Records<RowDataList[], number>>()
 const arr = ref<NodeList>()
 const visibleIndexs = ref<number[]>([-1, 0, 1])
 const currentIndex = ref(0)
-
 const isInVisiblePages = (visibleIndexs: number[], current: number) => {
   return visibleIndexs.indexOf(current) > -1
 }
-
 onMounted(() => {
   initScroll()
   watch(defaultArticles, handleArticleData)
   defaultArticles.value = [...defaultArticles.value, ...tem]
 })
-
-
 const handleArticleData = (list: RowDataList[]) => {
   const _list = [...list]
   const allList = []
@@ -190,11 +177,8 @@ const handleArticleData = (list: RowDataList[]) => {
       data: currentList
     })
   }
-
   handleWaterfullList(allList)
 }
-
-
 const handleWaterfullList = (list: FirstHandleData<RowDataList[]>[]) => {
   (list as SecondHandleData<RowDataList[]>[]).forEach((item: SecondHandleData<RowDataList[]>, index: number) => {
     const isLast = index + 1 === list.length
@@ -222,19 +206,14 @@ const handleWaterfullList = (list: FirstHandleData<RowDataList[]>[]) => {
         list[nextIndex].rightOffset = 0
       }
     }
-
     item.leftData = leftData
     item.rightData = rightData
     item.height = Math.max(leftHeight, rightHeight)
   })
-
   records.value = list as Records<RowDataList[], number>
 }
-
-
 const createObserve = () => {
   io.value = new IntersectionObserver(function (entries) {
-
     for (let entry in entries) {
       if (entries[entry].intersectionRatio > 0) {
         let index = Number(entries[entry].target.getAttribute('index'))
@@ -248,7 +227,6 @@ const createObserve = () => {
   })
 }
 createObserve()
-
 watch(records, () => {
   arr.value = document.querySelectorAll('.waterfull__item')
   nextTick(() => {
@@ -257,13 +235,22 @@ watch(records, () => {
     })
   })
 })
-
 const initScroll = () => {
   //初始化滑动事件监听器
   window.addEventListener('scroll', scrollListener)
 }
 
-
+function throttled(fn: any, delay = 500) {
+  let timer: any = null
+  return function (...args: any[]) {
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn()
+        timer = null
+      }, delay);
+    }
+  }
+}
 const scrollListener = () => {
   // screen.availHeight表示屏幕高度
   // document.documentElement.scrollTop表示当前页面滚动条的位置,documentElement对应的是html标签,body对应的是body标签
@@ -271,18 +258,18 @@ const scrollListener = () => {
   let scrollY = document.documentElement.scrollTop || document.body.scrollTop // 滚动条在Y轴上的滚动距离
   let vh = document.compatMode === 'CSS1Compat' ? document.documentElement.clientHeight : document.body.clientHeight // 页面的可视高度（能看见的）
   let allHeight = Math.max(document.body.scrollHeight, document.documentElement.scrollHeight) // 页面的总高度（所有的）
-  if ((scrollY + vh) >= (allHeight - 1)) { // 当滚动条滑到页面底部
-    defaultArticles.value = [...defaultArticles.value, ...tem]
-  }
+
+  throttled(() => {
+    if ((scrollY + vh) >= (allHeight - 1)) { // 当滚动条滑到页面底部
+      defaultArticles.value = [...defaultArticles.value, ...tem]
+    }
+  })()
 }
-
-
 onUnmounted(() => {
   //退出时销毁上滑加载监听的滚动事件
   window.removeEventListener('scroll', scrollListener)
   io.value?.disconnect()
 })
-
 </script>
 <style lang="less" scoped>
 .waterfull {
